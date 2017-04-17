c4f3a96429816d55e84f6acd6d6713957b73895e
Lucero07 <ing.lucero.ramos@gmail.com>
2017-04-17 12:14:52 -0500

    avance

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+var arregloImagenes = document.getElementsByClassName('imagenes');
+console.log(arregloImagenes);
+for(var i =0; i<arregloImagenes.lenght;i++){
+  arregloImagenes[i].addEventListener('click', mostrar);
+};
+
+function mostrar() {
+
+
+
+}
